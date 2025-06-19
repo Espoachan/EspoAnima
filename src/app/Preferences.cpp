8e7326249482f8c6f53cda8d6c5f0e8d15caa78f
Espoachan <leoponce200111@gmail.com>
2025-06-19 13:33:12 -0600

    Archivo Preferences.cpp ajustado a Preferences.h

--- src/app/Preferences.cpp
+++ src/app/Preferences.cpp
@@ -1,7 +1,17 @@
 #include "headers/Preferences.h"
 
-Preferences::Preferences(QObject *parent)
+Preferences::Preferences(QWidget *parent)
+    : QDialog(parent)
 {
-  
+    setWindowTitle("Preferences");
+    setWindowFlags(Qt::Dialog | Qt::WindowCloseButtonHint | Qt::WindowTitleHint);
+    setModal(true);
+    setMinimumSize(400, 300);
+
+    // Add your preference UI elements here
+    // For example, you can add buttons, checkboxes, etc.
+    QPushButton *closeButton = new QPushButton("Close", this);
+    connect(closeButton, &QPushButton::clicked, this, &Preferences::close);
+    closeButton->setGeometry(10, 10, 80, 30);
 }
 
